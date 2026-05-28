24afe4326eabfa7cc10ba8a003dcab107fa9b1d6
Mel-0 <73212655+Mel-0@users.noreply.github.com>
2026-05-27 21:24:19 -0600

    Mi Primer commit

---
+++ happy_time_balloons/HappyTimesBalloons.AccesoADatos/Repositorios/CatalogoProductoRepositorio.cs
@@ -0,0 +1,69 @@
+﻿using HappyTimesBalloons.Abstraccion.DTOs;
+using HappyTimesBalloons.Abstraccion.Interfaces.Repositorios;
+using HappyTimesBalloons.AccesoADatos.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyTimesBalloons.AccesoADatos.Repositorios
+{
+    public class CatalogoProductoRepositorio : ICatalogoProductoRepositorio
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CatalogoProductoRepositorio(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<CatalogoProductoDTO> ObtenerCatalogo()
+        {
+            return _context.Productos
+                .Include("Categoria")
+                .Include("Imagenes")
+                .Where(p => p.EsActivo)
+                .Select(p => new CatalogoProductoDTO
+                {
+                    ProductoId = p.Id,
+                    Nombre = p.Nombre,
+                    Descripcion = p.Descripcion,
+                    Precio = p.Precio,
+                    PrecioDescuento = p.PrecioDescuento,
+                    CategoriaNombre = p.Categoria.Nombre,
+                    ImagenPrincipal = p.Imagenes
+                        .Where(i => i.EsPrincipal)
+                        .Select(i => i.RutaImagen)
+                        .FirstOrDefault(),
+                    EsActivo = p.EsActivo,
+                    Disponible = p.Stock > 0
+                })
+                .ToList();
+        }
+
+        public CatalogoProductoDTO ObtenerProductoCatalogoPorId(int productoId)
+        {
+            return _context.Productos
+                .Include("Categoria")
+                .Include("Imagenes")
+                .Where(p => p.Id == productoId && p.EsActivo)
+                .Select(p => new CatalogoProductoDTO
+                {
+                    ProductoId = p.Id,
+                    Nombre = p.Nombre,
+                    Descripcion = p.Descripcion,
+                    Precio = p.Precio,
+                    PrecioDescuento = p.PrecioDescuento,
+                    CategoriaNombre = p.Categoria.Nombre,
+                    ImagenPrincipal = p.Imagenes
+                        .Where(i => i.EsPrincipal)
+                        .Select(i => i.RutaImagen)
+                        .FirstOrDefault(),
+                    EsActivo = p.EsActivo,
+                    Disponible = p.Stock > 0
+                })
+                .FirstOrDefault();
+        }
+    }
+}
